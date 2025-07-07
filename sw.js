643183b9d79685485283a92516a23c473a60d213
Lin <salin3010@gmail.com>
2025-07-07 14:27:18 +0800

    edited for PWA deployment

---
+++ sw.js
@@ -0,0 +1,57 @@
+// sw.js
+    const CACHE_NAME = 'my-app-cache-v1';
+    const urlsToCache = [
+      '/img/img1.png',
+      '/img/img2.png',
+      '/img/img3.png',
+      '/img/img4.png',
+      '/img/img5.png',
+      '/img/img6.png',
+      '/img/img7.png',
+      '/img/img8.png',
+      '/img/logo.png',
+      '/index.html',
+      '/fonts/Unageo-Regular.ttf',
+      '/confetti.browser.min.js',
+      '/icon512_rounded.png',
+      '/icon512_maskable.png'
+      // Add all other assets your app needs to work offline
+    ];
+
+    self.addEventListener('install', event => {
+      event.waitUntil(
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+          })
+      );
+    });
+
+    self.addEventListener('fetch', event => {
+      event.respondWith(
+        caches.match(event.request)
+          .then(response => {
+            // Cache hit - return response
+            if (response) {
+              return response;
+            }
+            return fetch(event.request);
+          })
+      );
+    });
+
+    self.addEventListener('activate', event => {
+      const cacheWhitelist = [CACHE_NAME];
+      event.waitUntil(
+        caches.keys().then(cacheNames => {
+          return Promise.all(
+            cacheNames.map(cacheName => {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            })
+          );
+        })
+      );
+    }); 
